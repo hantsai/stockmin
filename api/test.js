7931ede33ddc6483a83f1f26d186ed1b64f44a0e
MaxTsai <pc0612303@gmail.com>
2026-05-24 00:19:11 +0800

    Delete api/test.js

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const apiKey = process.env.ANTHROPIC_API_KEY;
-  res.status(200).json({ 
-    hasKey: !!apiKey,
-    keyPrefix: apiKey ? apiKey.substring(0, 10) + "..." : "none"
-  });
-}
